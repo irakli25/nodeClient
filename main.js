4f41ddf23260e08f8dc979a58009fdb558563968
irakli25 <tabukashvili_irakli@yahoo.com>
2024-09-18 13:48:34 +0400

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ signupButton.addEventListener("click", signup)
 loginButton.addEventListener("click", login)
 closeButton.addEventListener("click", () => modal.close())
 
-const SERVER_URL = "https://node-server-taupe-ten.vercel.app/api/auth"
+const SERVER_URL = "https://node-server-ten-drab.vercel.app/api/auth"
 
 async function signup() {
   const email = emailInput.value
